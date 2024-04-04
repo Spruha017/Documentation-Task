e81f61c51a8cd1ad71fa3c2879fa3c79269dfc92
Spruha Thorat <202151172@iiitvadodara.ac.in>
2024-04-04 23:57:07 +0530

    final

--- docum/docusaurus.config.js
+++ docum/docusaurus.config.js
@@ -68,7 +68,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'Documentation',
+        title: 'move to documentation ',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -78,9 +78,9 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'documentation',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          // {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
@@ -95,7 +95,7 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
+                label: 'documentation',
                 to: '/docs/intro',
               },
             ],
